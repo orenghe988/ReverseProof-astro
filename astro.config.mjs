a02971d75c0f4edefe0bf77a62770eb127c66516
Oren Ghelerter <orenghe@gmail.com>
2022-08-20 20:52:51 +0300

    commit uncomitted changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare'
+import cloudflare from '@astrojs/cloudflare';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: cloudflare({ mode: 'directory' })
+  output: 'static',
+  integrations: [react(), tailwind(), svelte()]
 });
